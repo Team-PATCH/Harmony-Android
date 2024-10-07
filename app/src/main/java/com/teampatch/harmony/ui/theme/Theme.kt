dd5d8862363048e8875441b84aa2c87461d17574
agvber <agvber12@gmail.com>
2024-10-08 02:11:51 +0900

    design: 컬러 색상 추가

--- app/src/main/java/com/teampatch/harmony/ui/theme/Theme.kt
+++ app/src/main/java/com/teampatch/harmony/ui/theme/Theme.kt
@@ -5,9 +5,9 @@ import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
+    primary = MainGreen,
+    secondary = Green2,
+    background = WH
 
     /* Other default colors to override
     background = Color(0xFFFFFBFE),
