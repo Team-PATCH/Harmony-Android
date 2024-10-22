e7909d904e89af6942c601bde17c254b19fa8e70
agvber <agvber12@gmail.com>
2024-10-22 18:45:30 +0900

    feat: 기본 AppBar 추가

--- core/designsystem/src/main/java/com/teampatch/core/designsystem/component/HomeAppBar.kt
+++ core/designsystem/src/main/java/com/teampatch/core/designsystem/component/HomeAppBar.kt
@@ -1,14 +1,9 @@
 package com.teampatch.core.designsystem.component
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.RowScope
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -21,21 +16,18 @@ fun HomeAppBar(
     modifier: Modifier = Modifier,
     actions: @Composable RowScope.() -> Unit
 ) {
-    Row(
-        verticalAlignment = Alignment.CenterVertically,
-        horizontalArrangement = Arrangement.SpaceBetween,
-        modifier = Modifier
-            .fillMaxWidth()
-            .height(70.dp)
-            .then(modifier)
-    ) {
-        Image(
-            painter = painterResource(R.drawable.ic_harmony_appbar),
-            contentDescription = "appbar_logo",
-            modifier = Modifier.padding(start = 28.dp)
-        )
-        actions()
-    }
+    AppBar(
+        navigation = {
+            Image(
+                painter = painterResource(R.drawable.ic_harmony_appbar),
+                contentDescription = "appbar_logo",
+                modifier = Modifier.padding(start = 28.dp)
+            )
+        },
+        title = { },
+        actions = { actions() },
+        modifier = modifier
+    )
 }
 
 @Preview(showBackground = true)
