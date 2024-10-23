e8b82a4df1c19945cd7ea2f31967f2a6510ab629
agvber <agvber12@gmail.com>
2024-10-24 00:47:26 +0900

    feat: 뒤로가기 버튼이 존재하는 Appbar 추가

--- core/designsystem/src/main/java/com/teampatch/core/designsystem/component/BackButtonAppBar.kt
+++ core/designsystem/src/main/java/com/teampatch/core/designsystem/component/BackButtonAppBar.kt
@@ -3,10 +3,13 @@ package com.teampatch.core.designsystem.component
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.RowScope
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.LocalTextStyle
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -18,8 +21,9 @@ import com.teampatch.core.designsystem.theme.PretendardFontFamily
 
 @Composable
 fun BackButtonAppBar(
+    onBackRequest: () -> Unit,
     modifier: Modifier = Modifier,
-    title: @Composable RowScope.() -> Unit = {},
+    title: @Composable () -> Unit = {},
     actions: @Composable RowScope.() -> Unit = {}
 ) {
     AppBar(
@@ -27,10 +31,25 @@ fun BackButtonAppBar(
             Image(
                 painter = painterResource(R.drawable.ic_back_appbar),
                 contentDescription = "back",
-                modifier = Modifier.padding(start = 24.dp)
+                modifier = Modifier
+                    .padding(start = 24.dp)
+                    .nonReplyClickable { onBackRequest() }
             )
         },
-        title = { title() },
+        title = {
+            CompositionLocalProvider(
+                LocalTextStyle provides LocalTextStyle.current.merge(
+                    TextStyle(
+                        fontSize = 20.sp,
+                        fontFamily = PretendardFontFamily,
+                        fontWeight = FontWeight.Bold,
+                        color = BL
+                    )
+                )
+            ) {
+                title()
+            }
+        },
         actions = { actions() },
         modifier = modifier
     )
@@ -41,14 +60,9 @@ fun BackButtonAppBar(
 private fun BackButtonAppBarPreview() {
     HarmonyTheme {
         BackButtonAppBar(
+            onBackRequest = { },
             title = {
-                Text(
-                    text = "가족 정보",
-                    fontSize = 20.sp,
-                    fontFamily = PretendardFontFamily,
-                    fontWeight = FontWeight.Bold,
-                    color = BL
-                )
+                Text(text = "가족 정보")
             },
             actions = {
                 Image(
