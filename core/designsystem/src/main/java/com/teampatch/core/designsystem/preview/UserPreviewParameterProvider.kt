ca582a6c97e0553720914534e20139bf0f6f1861
agvber <agvber12@gmail.com>
2024-10-28 23:01:22 +0900

    fix: 누락된 코드 추가

--- core/designsystem/src/main/java/com/teampatch/core/designsystem/preview/UserPreviewParameterProvider.kt
+++ core/designsystem/src/main/java/com/teampatch/core/designsystem/preview/UserPreviewParameterProvider.kt
@@ -6,16 +6,16 @@ import com.teampatch.core.domain.model.User
 
 class UserPreviewParameterProvider : PreviewParameterProvider<User> {
     override val values: Sequence<User> = sequenceOf(
-        User("uid001", "Alice Johnson", null, Role.VIP),
-        User("uid002", "Bob Smith", null, Role.MEMBER),
-        User("uid003", "Charlie Brown", null, Role.VIP),
-        User("uid004", "Dana White", null, Role.VIP),
-        User("uid005", "Eve Black", null, Role.MEMBER),
-        User("uid006", "Frank Green", null, Role.MEMBER),
-        User("uid007", "Grace Lee", null, Role.MEMBER),
-        User("uid008", "Hank Miller", null, Role.VIP),
-        User("uid009", "Ivy Wilson", null, Role.VIP),
-        User("uid010", "Jack King", null, Role.VIP)
+        User("uid001", "Alice Johnson", "Mother", null, Role.VIP),
+        User("uid002", "Bob Smith", "Father", null, Role.MEMBER),
+        User("uid003", "Charlie Brown", "Older Brother", null, Role.MEMBER),
+        User("uid004", "Dana White", "Older Sister", null, Role.MEMBER),
+        User("uid005", "Eve Black", "Younger Sister", null, Role.MEMBER),
+        User("uid006", "Frank Green", "Younger Brother", null, Role.MEMBER),
+        User("uid007", "Grace Lee", "Aunt", null, Role.MEMBER),
+        User("uid008", "Hank Miller", "Uncle", null, Role.MEMBER),
+        User("uid009", "Ivy Wilson", "Grandmother", null, Role.MEMBER),
+        User("uid010", "Jack King", "Grandfather", null, Role.MEMBER)
     )
 
 }
--- feature/family-info/src/main/java/com/teampatch/feature/family/info/model/FamilyInfoUiState.kt
+++ feature/family-info/src/main/java/com/teampatch/feature/family/info/model/FamilyInfoUiState.kt
@@ -5,7 +5,7 @@ import com.teampatch.core.domain.model.Role
 import com.teampatch.core.domain.model.User
 
 data class FamilyInfoUiState(
-    val user: User = User(uid = "", name = "", profileImageUrl = null, role = Role.MEMBER),
+    val user: User = User(uid = "", name = "", profileImageUrl = null, relation = "", role = Role.MEMBER),
     val familyInfo: List<FamilyInfo> = emptyList(),
     val isLoading: Boolean = true,
 )
