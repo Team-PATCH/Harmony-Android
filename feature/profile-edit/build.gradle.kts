eb4a257171a1853ba6ebe76fa724376ebb18f90a
agvber <agvber12@gmail.com>
2024-10-29 18:29:58 +0900

    chore: Compose ui testing 추가

--- feature/profile-edit/build.gradle.kts
+++ feature/profile-edit/build.gradle.kts
@@ -20,4 +20,5 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+    androidTestImplementation(libs.androidx.ui.test.junit4.android)
 }
