b1d5879604a4136d5e5c546a22016b75dda12a3e
theBettor <be0128st@gmail.com>
2024-11-05 21:57:24 +0900

    refactor: LoginScreen, PermissionNotificationScreen 화면 분리

--- feature/onboarding/login/src/main/java/com/teampatch/feature/onboarding/login/ui/OnboardingScreen.kt
+++ feature/onboarding/login/src/main/java/com/teampatch/feature/onboarding/login/ui/OnboardingScreen.kt
@@ -81,120 +81,7 @@ import com.teampatch.feature.onboarding.login.R.string.subtext_onboarding_invite
 import com.teampatch.feature.onboarding.login.R.string.subtext_onboarding_make_space
 import com.teampatch.feature.onboarding.login.R.string.subtext_onboarding_setting_prfile_image
 
-@Composable
-fun LoginScreen() {
-    Scaffold(
-        modifier = Modifier
-            .fillMaxSize()
-            .background(Color(0xFFF5F5F5)),
-    ) { paddingValues ->
-        Box(
-            modifier = Modifier
-                .fillMaxSize()
-                .padding(paddingValues) // Scaffold의 기본 패딩
-                .padding(top = 135.dp, bottom = 8.dp) // Column 시작 위치에 추가 패딩
-        ) {
-            Column(
-                modifier = Modifier
-                    .fillMaxSize(),
-//                verticalArrangement = Arrangement.Center,
-//                verticalArrangement = Arrangement.Top,
-                verticalArrangement = Arrangement.SpaceBetween, // 첫 요소는 위, 마지막 요소는 아래에 붙음
-                horizontalAlignment = Alignment.CenterHorizontally
-            ) {
-                Image(
-                    painter = painterResource(id = img_logo_in_login),
-                    contentDescription = "Logo Harmony",
-                    modifier = Modifier
-                        .fillMaxWidth()
-                )
 
-                Spacer(modifier = Modifier.height(465.dp))
-
-                Button(
-                    onClick = { /* Handle Kakao Login */ },
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(horizontal = 20.dp)
-                        .height(68.dp),
-                    colors = ButtonDefaults.buttonColors(containerColor = Color(0xFFFFE812)), // 배경색은 이미지를 꽉 채우면 안 보이게 됩니다.
-                    shape = RoundedCornerShape(10.dp)
-                ) {
-                    // 이미지 리소스를 painterResource로 불러오고 버튼을 꽉 채움
-                    Image(
-                        painter = painterResource(id = R.drawable.kakao_login_medium_wide), // 카카오 로그인 이미지
-                        contentDescription = "Kakao Login",
-                        modifier = Modifier.fillMaxSize(), // 버튼 크기를 꽉 채움
-//                    contentScale = ContentScale.Crop // 이미지가 버튼에 맞게 잘리거나 확장됨
-//                    modifier = Modifier.fillMaxHeight(), // 버튼 높이에 맞게 이미지 채우기
-                        contentScale = ContentScale.Fit // 이미지가 잘리지 않고 버튼 안에 맞춰짐
-                    )
-                }
-            }
-        }
-    }
-}
-
-@Composable
-fun PermissionNotificationScreen() {
-    Scaffold(
-        modifier = Modifier
-            .fillMaxSize()
-            .background(Color(0xFFF5F5F5)),
-    ) { paddingValues ->
-        Box(
-            modifier = Modifier
-                .fillMaxSize()
-                .padding(paddingValues)
-                .padding(top = 92.dp, bottom = 8.dp) // Column 시작 위치에 추가 패딩
-        ) {
-            Column(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .padding(paddingValues),
-                verticalArrangement = Arrangement.SpaceBetween, // 첫 요소는 위, 마지막 요소는 아래에 붙음
-                horizontalAlignment = Alignment.CenterHorizontally
-            ) {
-                Image(
-                    painter = painterResource(id = com.teampatch.core.designsystem.R.drawable.image_permission_to_notify),
-                    contentDescription = "Permission Notification Image",
-                    modifier = Modifier
-                        .fillMaxWidth()
-                )
-
-                Spacer(modifier = Modifier.height(25.dp))
-
-                Image(
-                    painter = painterResource(id = com.teampatch.core.designsystem.R.drawable.img_character_fullbody_mony),
-                    contentDescription = "Full-Body Mony Character",
-                    modifier = Modifier
-                        .fillMaxWidth()
-                )
-
-                Spacer(modifier = Modifier.height(36.dp))
-
-                Button(
-                    onClick = { /* Handle Start Process */ },
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(horizontal = 20.dp)
-                        .height(68.dp),
-                    colors = ButtonDefaults.buttonColors(containerColor = Color.Transparent), // 투명한 배경
-                    contentPadding = PaddingValues(0.dp), // 버튼의 기본 내부 패딩 제거
-                    shape = RoundedCornerShape(10.dp)
-                ) {
-                    // 이미지 리소스를 painterResource로 불러오고 버튼을 꽉 채움
-                    Image(
-                        painter = painterResource(id = R.drawable.btn_start_harmony), // 카카오 로그인 이미지
-                        contentDescription = "Harmoy Start Process",
-                        modifier = Modifier.fillMaxSize(), // 이미지가 버튼의 크기를 꽉 채움
-                        contentScale = ContentScale.Crop // 이미지가 버튼 크기에 맞춰 잘림
-                    )
-                }
-            }
-        }
-    }
-}
 
 
 @Composable
@@ -706,17 +593,9 @@ fun EnterSpaceInSingularState() {
 //    )
 //}
 
-@Preview(showBackground = true)
-@Composable
-fun LoginScreenPreview() {
-    LoginScreen()
-}
 
-@Preview(showBackground = true)
-@Composable
-fun PermissionNotificationScreenPreview() {
-    PermissionNotificationScreen()
-}
+
+
 
 @Preview(showBackground = true)
 @Composable
