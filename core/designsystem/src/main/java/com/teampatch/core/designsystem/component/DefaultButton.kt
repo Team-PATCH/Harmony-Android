0bc17e1429a8a9baaf6256adfdb9494b5901a168
agvber <agvber12@gmail.com>
2024-11-05 23:14:57 +0900

    refactor: DefaultButton clickable code refactor

--- core/designsystem/src/main/java/com/teampatch/core/designsystem/component/DefaultButton.kt
+++ core/designsystem/src/main/java/com/teampatch/core/designsystem/component/DefaultButton.kt
@@ -38,11 +38,7 @@ fun DefaultButton(
             .widthIn(min = 80.dp)
             .heightIn(min = 68.dp)
             .background(if (enable) MainGreen else G2, RoundedCornerShape(10.dp))
-            .nonReplyClickable {
-                if (enable) {
-                    onClick()
-                }
-            }
+            .nonReplyClickable(enabled = enable, onClick = onClick)
     ) {
         CompositionLocalProvider(
             LocalTextStyle provides LocalTextStyle.current.merge(
