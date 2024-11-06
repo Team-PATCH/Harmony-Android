1a2d03ab3b6718264f1e6f99c16923cae796d097
theBettor <be0128st@gmail.com>
2024-11-06 23:21:36 +0900

    chore: 화면 구성 중

---
+++ feature/daily/src/main/java/com/teampatch/harmony/DailyManageScreen.kt
@@ -0,0 +1,26 @@
+package com.teampatch.harmony
+
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.teampatch.core.designsystem.theme.HarmonyTheme
+
+@Composable
+fun DailyManageScreen(
+    // TODO: Route
+) {
+    // TODO:  ??
+    Scaffold(
+        // 천천히 만들어보자~ 한 화면에~
+    ) { scaffoldPaddingValues ->
+
+    }
+}
+
+@Preview
+@Composable
+private fun DailyManageScreenPreview() {
+    HarmonyTheme {
+        DailyManageScreen()
+    }
+}
---
+++ feature/daily/src/main/java/com/teampatch/harmony/NotificationDailyScreen.kt
@@ -0,0 +1,18 @@
+package com.teampatch.harmony
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.teampatch.core.designsystem.theme.HarmonyTheme
+
+@Composable
+fun NotificationDailyScreen() {
+
+}
+
+@Preview
+@Composable
+private fun NotificationDailyScreenPreview() {
+    HarmonyTheme {
+        NotificationDailyScreen()
+    }
+}
