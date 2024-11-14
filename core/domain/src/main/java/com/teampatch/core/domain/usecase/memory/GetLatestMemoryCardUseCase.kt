c27956bc4127204736d98b2f13955fa0f583baa7
agvber <agvber12@gmail.com>
2024-11-14 21:28:41 +0900

    rename: GetMemoryCardUseCase -> GetLatestMemoryCardUseCase class 이름 변경

--- core/domain/src/main/java/com/teampatch/core/domain/usecase/memory/GetLatestMemoryCardUseCase.kt
+++ core/domain/src/main/java/com/teampatch/core/domain/usecase/memory/GetLatestMemoryCardUseCase.kt
@@ -5,7 +5,7 @@ import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.emptyFlow
 import javax.inject.Inject
 
-class GetMemoryCardUseCase @Inject constructor(){
+class GetLatestMemoryCardUseCase @Inject constructor(){
 
     operator fun invoke(): Flow<MemoryCard> {
         return emptyFlow()
