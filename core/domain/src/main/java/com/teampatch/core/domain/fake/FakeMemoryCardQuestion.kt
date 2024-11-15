eb6cbf0cff0ea0366338371639676208b40dc1b6
agvber <agvber12@gmail.com>
2024-11-16 03:52:17 +0900

    feat: MemoryCardQuestion Model 추가

---
+++ core/domain/src/main/java/com/teampatch/core/domain/fake/FakeMemoryCardQuestion.kt
@@ -0,0 +1,12 @@
+package com.teampatch.core.domain.fake
+
+import com.teampatch.core.domain.model.MemoryCardQuestion
+
+class FakeMemoryCardQuestion : FakeModel<MemoryCardQuestion>() {
+    override fun build(): MemoryCardQuestion {
+        return MemoryCardQuestion(
+            question = "다은이를 분만실에서 처음 봤을 때 어떤 느낌이 들었나요?",
+            last = true
+        )
+    }
+}
