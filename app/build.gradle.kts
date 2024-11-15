162f2b5a94234f56b1b0cb7a23c9c5c5bde57b9a
agvber <agvber12@gmail.com>
2024-11-16 03:59:57 +0900

    chore: app module data 의존성 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -55,6 +55,7 @@ android {
 
 dependencies {
 
+    implementation(project(":core:data"))
     implementation(project(":core:designsystem"))
     implementation(project(":feature:home"))
     implementation(project(":feature:onboarding"))
