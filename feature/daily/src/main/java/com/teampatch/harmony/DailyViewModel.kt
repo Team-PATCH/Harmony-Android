05b967366c07bd586d040753e9d5bd412680ba27
theBettor <be0128st@gmail.com>
2024-11-18 22:04:02 +0900

    feat: daily 화면

---
+++ feature/daily/src/main/java/com/teampatch/harmony/DailyViewModel.kt
@@ -0,0 +1,44 @@
+package com.teampatch.harmony
+
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.teampatch.core.domain.usecase.daily.GetDailyUseCase
+import com.teampatch.core.domain.usecase.question.GetQuestionsUseCase
+import com.teampatch.core.domain.usecase.user.GetUserInfoUseCase
+import com.teampatch.harmony.model.DailySideEffect
+import com.teampatch.harmony.model.DailyUiState
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.receiveAsFlow
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+internal class DailyViewModel @Inject constructor(
+    private val getUserInfoUseCase: GetUserInfoUseCase,
+    private val getDailyUseCase: GetDailyUseCase
+) : ViewModel() {
+
+    var dailyUiState = mutableStateOf(DailyUiState())
+        private set
+
+    private val _sideEffect = Channel<DailySideEffect>()
+    val sideEffect = _sideEffect.receiveAsFlow()
+
+    init {
+        load()
+    }
+
+    private fun load() = viewModelScope.launch {
+        try {
+            val user = getUserInfoUseCase().first()
+            val daily = getDailyUseCase()
+            dailyUiState.value = DailyUiState(user = user, daily = daily, isLoading = false)
+        } catch (e: Exception) {
+            _sideEffect.send(DailySideEffect.LoadError(e))
+            e.printStackTrace()
+        }
+    }
+}
