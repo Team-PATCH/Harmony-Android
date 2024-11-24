59f881ee1ef03bbe308479d3e2e8cfee77b51425
agvber <agvber12@gmail.com>
2024-11-25 01:56:25 +0900

    chore: network module 추가

---
+++ core/network/build.gradle.kts
@@ -0,0 +1,21 @@
+plugins {
+    id("teampatch.android.library")
+    id("teampatch.android.hilt")
+}
+
+android {
+    namespace = "com.teampatch.core.network"
+}
+
+dependencies {
+
+    implementation(project(":core:tokenstore"))
+
+    implementation(libs.squareup.retrofit2)
+    implementation(libs.squareup.retrofit2.converter.moshi)
+    implementation(libs.squareup.moshi.kotlin)
+
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+}
