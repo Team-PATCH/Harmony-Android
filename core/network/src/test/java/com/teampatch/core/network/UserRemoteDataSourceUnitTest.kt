f2e43541d52267effefff91b81d58cf033bb8b54
agvber <agvber12@gmail.com>
2024-12-01 01:29:45 +0900

    test: Network User 테스트 코드 작성

--- core/network/src/test/java/com/teampatch/core/network/UserRemoteDataSourceUnitTest.kt
+++ core/network/src/test/java/com/teampatch/core/network/UserRemoteDataSourceUnitTest.kt
@@ -1,6 +1,7 @@
 package com.teampatch.core.network
 
 import com.teampatch.core.network.di.NetworkSingletonModule
+import com.teampatch.core.network.model.user.SignupOrLoginRequestBody
 import kotlinx.coroutines.runBlocking
 import okhttp3.OkHttpClient
 import org.junit.Before
@@ -12,6 +13,8 @@ class UserRemoteDataSourceUnitTest {
     private lateinit var okHttpClient: OkHttpClient
     private lateinit var retrofit: Retrofit
     private lateinit var userRemoteDataSource: UserRemoteDataSource
+
+    private val token = ""
 
     @Before
     fun setup() {
@@ -25,7 +28,7 @@ class UserRemoteDataSourceUnitTest {
             .addInterceptor { chain ->
                 val request = chain.request()
                 val newReq = request.newBuilder()
-                    .addHeader("Authorization", "Bearer Token") // TODO: 토큰 입력하기
+                    .addHeader("Authorization", "Bearer $token") // TODO: 토큰 입력하기
                     .build()
                 chain.proceed(newReq)
             }
@@ -44,7 +47,24 @@ class UserRemoteDataSourceUnitTest {
     }
 
     @Test
-    fun `로그인_테스트`(): Unit = runBlocking {
-        userRemoteDataSource.getMyProfile()
+    fun `회원가입&로그인_테스트`(): Unit = runBlocking {
+        val body = SignupOrLoginRequestBody(
+            "yeojeong@naver.com",
+            "윤여정",
+            "profile.png",
+            "kakao",
+            "kakao_social_token_example",
+            "kakao_refresh_token_example",
+            "2024-08-08 02:44:07"
+
+        )
+        val response = userRemoteDataSource.signupOrLogin(body)
+        println("회원가입&로그인_테스트: $response")
+    }
+
+    @Test
+    fun `사용자_정보_조회_테스트`(): Unit = runBlocking {
+        val response = userRemoteDataSource.getMyProfile()
+        println("사용자_정보_조회_테스트: $response")
     }
 }
