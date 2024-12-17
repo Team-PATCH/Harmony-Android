32d7b508b9c65a8c8576048fd2c177696b80961b
agvber <agvber12@gmail.com>
2024-12-17 21:48:27 +0900

    fix: SignupOrLoginResponse 값이 일치하지 않는 문제 수정

--- core/network/src/main/java/com/teampatch/core/network/model/user/SignupOrLoginResponse.kt
+++ core/network/src/main/java/com/teampatch/core/network/model/user/SignupOrLoginResponse.kt
@@ -7,13 +7,12 @@ import com.squareup.moshi.JsonClass
 data class SignupOrLoginResponse(
     @Json(name = "message") val message: String,
     @Json(name = "user") val user: User,
-    @Json(name = "permissionId") val permissionId: String?,
     @Json(name = "token") val token: String,
 ) {
     data class User(
         @Json(name = "nick") val nick: String,
         @Json(name = "authProvider") val authProvider: String,
         @Json(name = "groupId") val groupId: String,
-        @Json(name = "permissionId") val permissionId: String,
+        @Json(name = "permissionId") val permissionId: String?,
     )
 }
