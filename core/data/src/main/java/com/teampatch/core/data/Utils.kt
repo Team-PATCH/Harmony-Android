739ccb258f0bcca7a3e0409dbba6898c9f97e35d
agvber <agvber12@gmail.com>
2024-12-19 00:42:39 +0900

    refactor: toServerDateFormat 함수 재사용성을 위해 utils 패키지에 리펙토링 https://github.com/Team-PATCH/Harmony-Android/pull/48#discussion_r1888580235

--- core/data/src/main/java/com/teampatch/core/data/Utils.kt
+++
@@ -1,5 +0,0 @@
-package com.teampatch.core.data
-
-import java.time.format.DateTimeFormatter
-
-internal val serverDateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'")
--- core/data/src/main/java/com/teampatch/core/data/repository/AuthenticationRepositoryImpl.kt
+++ core/data/src/main/java/com/teampatch/core/data/repository/AuthenticationRepositoryImpl.kt
@@ -1,15 +1,12 @@
 package com.teampatch.core.data.repository
 
 import com.agvber.core.authentication.kakao.KakaoLoginService
-import com.teampatch.core.data.serverDateTimeFormatter
+import com.teampatch.core.data.utils.toServerDateFormat
 import com.teampatch.core.domain.entity.TokenManager
 import com.teampatch.core.domain.model.LoginResult
 import com.teampatch.core.domain.repository.AuthenticationRepository
 import com.teampatch.core.network.UserRemoteDataSource
 import com.teampatch.core.network.model.user.SignupOrLoginRequestBody
-import java.time.LocalDateTime
-import java.time.ZoneId
-import java.util.Date
 import javax.inject.Inject
 
 class AuthenticationRepositoryImpl @Inject constructor(
@@ -34,13 +31,5 @@ class AuthenticationRepositoryImpl @Inject constructor(
         tokenManager.setAccessToken(signupOrLoginResponse.token)
 
         return LoginResult(groupId = signupOrLoginResponse.user.groupId)
-    }
-
-    private fun Date.toServerDateFormat(): String {
-        return LocalDateTime.ofInstant(
-            toInstant(),
-            ZoneId.systemDefault()
-        )
-            .format(serverDateTimeFormatter)
     }
 }
