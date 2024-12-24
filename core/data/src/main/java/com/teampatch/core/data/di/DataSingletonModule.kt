cf798275df1e5d3d22240ab967ea8bad30d8cafa
agvber <agvber12@gmail.com>
2024-12-24 20:17:33 +0900

    refactor: TokenManager Hilt 인스턴스 생성 시점 변경 (ViewModel -> Application)

--- core/data/src/main/java/com/teampatch/core/data/di/DataSingletonModule.kt
+++ core/data/src/main/java/com/teampatch/core/data/di/DataSingletonModule.kt
@@ -1,6 +1,8 @@
 package com.teampatch.core.data.di
 
+import com.teampatch.core.data.entity.TokenManagerImpl
 import com.teampatch.core.data.repository.UserRepositoryImpl
+import com.teampatch.core.domain.entity.TokenManager
 import com.teampatch.core.domain.repository.UserRepository
 import dagger.Binds
 import dagger.Module
@@ -10,6 +12,11 @@ import dagger.hilt.components.SingletonComponent
 @Module
 @InstallIn(SingletonComponent::class)
 abstract class DataSingletonModule {
+
+    @Binds
+    abstract fun bindsTokenManager(
+        tokenManagerImpl: TokenManagerImpl
+    ): TokenManager
 
     @Binds
     abstract fun bindsUserRepository(
