1c255febf368a9e2abe1d4602011a4fe8ea358ea
agvber <agvber12@gmail.com>
2024-12-27 18:44:53 +0900

    feat: FakeTodos 더미 데이터 추가

--- core/domain/src/main/java/com/teampatch/core/domain/usecase/routine/GetDailyRoutineUseCase.kt
+++ core/domain/src/main/java/com/teampatch/core/domain/usecase/routine/GetDailyRoutineUseCase.kt
@@ -1,14 +1,15 @@
 package com.teampatch.core.domain.usecase.routine
 
 import androidx.paging.PagingData
+import com.teampatch.core.domain.fake.FakeTodos
 import com.teampatch.core.domain.model.Todo
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.emptyFlow
+import kotlinx.coroutines.flow.flowOf
 import javax.inject.Inject
 
 class GetDailyRoutineUseCase @Inject constructor() {
 
     operator fun invoke(): Flow<PagingData<Todo>> {
-        return emptyFlow()
+        return flowOf(PagingData.from(FakeTodos().get()))
     }
 }
