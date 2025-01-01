18d5d61680626bd66b23de3ba62f02c3b5dd7349
agvber <agvber12@gmail.com>
2025-01-01 18:14:38 +0900

    chore: app common module 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -107,6 +107,7 @@ fun DefaultConfig.setBuildConfigOrManifest(
 
 dependencies {
 
+    implementation(project(":core:common"))
     implementation(project(":core:authentication"))
     implementation(project(":core:data"))
     implementation(project(":core:domain"))
