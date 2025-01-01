1beba385d60ef738d4647c52078f444ea87f83b1
agvber <agvber12@gmail.com>
2025-01-02 02:44:17 +0900

    feat: QuestionDetail model comment 추가

--- core/domain/src/main/java/com/teampatch/core/domain/model/QuestionDetail.kt
+++ core/domain/src/main/java/com/teampatch/core/domain/model/QuestionDetail.kt
@@ -1,6 +1,9 @@
 package com.teampatch.core.domain.model
 
+import androidx.paging.PagingData
 import java.time.LocalDateTime
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flowOf
 
 data class QuestionDetail(
     val id: String,
@@ -9,4 +12,5 @@ data class QuestionDetail(
     val content: String,
     val dateTime: LocalDateTime,
     val commentCount: Int,
+    val comment: Flow<PagingData<QuestionComment>> = flowOf(PagingData.empty()),
 )
