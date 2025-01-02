974480d74a7b4db8f1254e08bbf1b458921b245a
agvber <agvber12@gmail.com>
2025-01-03 04:59:26 +0900

    feat: answer domain 로직 Network API 연결

--- core/domain/src/main/java/com/teampatch/core/domain/usecase/answer/EditAnswerUseCase.kt
+++ core/domain/src/main/java/com/teampatch/core/domain/usecase/answer/EditAnswerUseCase.kt
@@ -1,12 +1,16 @@
 package com.teampatch.core.domain.usecase.answer
 
+import com.teampatch.core.domain.repository.AnswerRepository
 import javax.inject.Inject
 
-class EditAnswerUseCase @Inject constructor() {
+class EditAnswerUseCase @Inject constructor(
+    private val answerRepository: AnswerRepository,
+) {
 
     suspend operator fun invoke(
         questionId: String,
         answer: String,
     ) {
+        answerRepository.editAnswer(questionId, answer)
     }
 }
