7e27f874a8874f6eccad9afaf13e9b3985d7c16b
agvber <agvber12@gmail.com>
2025-01-06 18:04:00 +0900

    design: BottomNavigation 화면에 따른 숨김 처리

--- app/src/main/java/com/teampatch/harmony/MainApp.kt
+++ app/src/main/java/com/teampatch/harmony/MainApp.kt
@@ -1,5 +1,9 @@
 package com.teampatch.harmony
 
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
@@ -9,7 +13,10 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.navigation.NavBackStackEntry
@@ -22,40 +29,62 @@ import com.teampatch.feature.home.navigateToHomeScreen
 import com.teampatch.feature.question.QuestionRoute
 import com.teampatch.feature.question.navigateToQuestionScreen
 
+private val BottomNavigationEnableScreens: Set<String?> = hashSetOf(
+    HomeRoute::class.qualifiedName,
+    QuestionRoute::class.qualifiedName
+)
+
 @Composable
 fun MainApp() {
     val navController: NavHostController = rememberNavController()
-    val currentBackStackEntry: NavBackStackEntry? by
-        navController.currentBackStackEntryFlow.collectAsStateWithLifecycle(null)
+    val currentBackStackEntry: NavBackStackEntry? by navController.currentBackStackEntryFlow.collectAsStateWithLifecycle(
+        initialValue = null
+    )
+    var previousNavigationItem by rememberSaveable { mutableStateOf(NavigationItem.HOME) }
     val navigationItem: NavigationItem by remember(currentBackStackEntry) {
-        derivedStateOf {
+        mutableStateOf(
             when (currentBackStackEntry?.destination?.route) {
                 HomeRoute::class.qualifiedName -> {
-                    NavigationItem.HOME
+                    NavigationItem.HOME.also {
+                        previousNavigationItem = it
+                    }
                 }
 
                 QuestionRoute::class.qualifiedName -> {
-                    NavigationItem.QUESTION
+                    NavigationItem.QUESTION.also {
+                        previousNavigationItem = it
+                    }
                 }
 
-                else -> NavigationItem.HOME
+                else -> previousNavigationItem
             }
+        )
+    }
+    val isBottomNavigationShow: Boolean by remember(currentBackStackEntry) {
+        derivedStateOf {
+            currentBackStackEntry?.destination?.route in BottomNavigationEnableScreens
         }
     }
 
     Scaffold(
         bottomBar = {
-            DefaultBottomNavigation(
-                onClick = {
-                    when (it) {
-                        NavigationItem.HOME -> navController.navigateToHomeScreen()
-                        NavigationItem.STORE -> {}
-                        NavigationItem.QUESTION -> navController.navigateToQuestionScreen()
-                        NavigationItem.CALENDAR -> {}
-                    }
-                },
-                navigationItem = navigationItem
-            )
+            AnimatedVisibility(
+                visible = isBottomNavigationShow,
+                enter = fadeIn(tween(400)),
+                exit = fadeOut(tween(400))
+            ) {
+                DefaultBottomNavigation(
+                    onClick = {
+                        when (it) {
+                            NavigationItem.HOME -> navController.navigateToHomeScreen()
+                            NavigationItem.STORE -> {}
+                            NavigationItem.QUESTION -> navController.navigateToQuestionScreen()
+                            NavigationItem.CALENDAR -> {}
+                        }
+                    },
+                    navigationItem = navigationItem
+                )
+            }
         },
         modifier = Modifier
             .fillMaxSize()
@@ -65,8 +94,7 @@ fun MainApp() {
     ) { scaffoldPaddingValue ->
         MainNavHost(
             navController = navController,
-            modifier = Modifier
-                .padding(scaffoldPaddingValue)
+            modifier = Modifier.padding(scaffoldPaddingValue)
         )
     }
 }
