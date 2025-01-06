33667ae707584b6393dce5f4fc0769cf514b8178
agvber <agvber12@gmail.com>
2025-01-06 18:49:16 +0900

    refactor: GetQuestionsUseCase limit 기본 상수 값 추가 https://github.com/Team-PATCH/Harmony-Android/pull/81#discussion_r1903113479

--- core/domain/src/main/java/com/teampatch/core/domain/usecase/question/GetQuestionsUseCase.kt
+++ core/domain/src/main/java/com/teampatch/core/domain/usecase/question/GetQuestionsUseCase.kt
@@ -6,9 +6,11 @@ import com.teampatch.core.domain.repository.QuestionRepository
 import javax.inject.Inject
 import kotlinx.coroutines.flow.Flow
 
+private const val QUESTION_LIMIT_COUNT = 3
+
 class GetQuestionsUseCase @Inject constructor(
     private val questionRepository: QuestionRepository,
 ) {
 
-    operator fun invoke(limit: Int = 3): Flow<PagingData<Question>> = questionRepository.getQuestions(limit)
+    operator fun invoke(limit: Int = QUESTION_LIMIT_COUNT): Flow<PagingData<Question>> = questionRepository.getQuestions(limit)
 }
