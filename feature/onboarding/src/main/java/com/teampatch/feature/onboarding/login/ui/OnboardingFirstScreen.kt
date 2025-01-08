ab39a8f127d57fbe5271925c4d2898792fb5ce90
theBettor <be0128st@gmail.com>
2025-01-08 18:50:43 +0900

    refactor: 누락된 로직 추가
    로그인o / 권한o 일때 StartScreen으로 보내는 로직 추가

--- feature/onboarding/src/main/java/com/teampatch/feature/onboarding/login/ui/OnboardingFirstScreen.kt
+++ feature/onboarding/src/main/java/com/teampatch/feature/onboarding/login/ui/OnboardingFirstScreen.kt
@@ -47,8 +47,10 @@ internal fun OnboardingFirstScreen(
         val hasNotificationGranted = ContextCompat.checkSelfPermission(context, Manifest.permission.POST_NOTIFICATIONS) == PackageManager.PERMISSION_GRANTED
         if (isLoginSuccessful && !hasNotificationGranted) {
             onPermissionNotificationRequest()
-        } // 지금 뷰모델에선 false로 초기화하고 있고 추측하기로는 !가 반대를 의미하니까 true라고 생각했는데
-        // 그게 아니라 false라면 반대로 밑에서도 !를 넣어서 false라고 한다면?
+        }
+        if (isLoginSuccessful && hasNotificationGranted) {
+            onStartScreenRequest()
+        }
     }
 
     Scaffold(
