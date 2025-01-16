0d6967dc1566f3a25bd709ca8137c1ccc0fe05be
agvber <agvber12@gmail.com>
2025-01-16 16:13:29 +0900

    feat: 그룹 정보 화면 user 값이 변경되는 경우 값을 stream 형태로 동기화

--- feature/family-info/src/main/java/com/teampatch/feature/family/info/FamilyInfoViewModel.kt
+++ feature/family-info/src/main/java/com/teampatch/feature/family/info/FamilyInfoViewModel.kt
@@ -13,7 +13,8 @@ import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
-import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.receiveAsFlow
 import kotlinx.coroutines.launch
 
@@ -36,13 +37,13 @@ internal class FamilyInfoViewModel @Inject constructor(
 
     private fun load() = viewModelScope.launch {
         try {
-            val user = getUserInfoUseCase().first()
-            val familyInfo = getFamilyInfoUseCase().first()
-            _familyInfoUiState.value = FamilyInfoUiState(
-                user = user,
-                familyInfo = familyInfo,
-                isLoading = false
-            )
+            combine(getUserInfoUseCase(), getFamilyInfoUseCase()) { user, familyInfo ->
+                _familyInfoUiState.value = FamilyInfoUiState(
+                    user = user,
+                    familyInfo = familyInfo,
+                    isLoading = false
+                )
+            }.launchIn(this)
         } catch (e: Exception) {
             e.printStackTrace()
             _sideEffect.send(FamilyInfoSideEffect.LoadError(e))
