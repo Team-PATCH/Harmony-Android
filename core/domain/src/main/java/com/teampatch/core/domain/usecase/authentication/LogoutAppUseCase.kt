d48ab54baaf02579edda99514ce1382f849cc032
agvber <agvber12@gmail.com>
2025-01-25 01:40:55 +0900

    refactor: 로그아웃 기능 domain|data layer 코드 정리

--- core/domain/src/main/java/com/teampatch/core/domain/usecase/authentication/LogoutAppUseCase.kt
+++ core/domain/src/main/java/com/teampatch/core/domain/usecase/authentication/LogoutAppUseCase.kt
@@ -1,16 +1,13 @@
 package com.teampatch.core.domain.usecase.authentication
 
-import com.teampatch.core.domain.entity.TokenManager
 import com.teampatch.core.domain.repository.AuthenticationRepository
 import javax.inject.Inject
 
 class LogoutAppUseCase @Inject constructor(
-    private val tokenManager: TokenManager,
     private val authenticationRepository: AuthenticationRepository,
 ) {
 
     suspend operator fun invoke() {
         authenticationRepository.logout()
-        tokenManager.setAccessToken("")
     }
 }
