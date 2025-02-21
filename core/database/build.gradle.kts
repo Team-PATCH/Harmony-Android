efcfe9a6742a16b2856109ef7965219c769681b5
agvber <agvber12@gmail.com>
2025-02-21 14:36:24 +0900

    chore: room 의존성 추가

--- core/database/build.gradle.kts
+++ core/database/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("teampatch.android.library")
+    id("com.google.devtools.ksp")
 }
 
 android {
@@ -7,6 +8,20 @@ android {
 }
 
 dependencies {
+
+    implementation(libs.androidx.room.runtime)
+    ksp(libs.androidx.room.compiler)
+    annotationProcessor(libs.androidx.room.compiler)
+
+    // optional - Kotlin Extensions and Coroutines support for Room
+    implementation(libs.androidx.room.ktx)
+
+    // optional - Test helpers
+    testImplementation(libs.androidx.room.testing)
+
+    // optional - Paging 3 Integration
+    implementation(libs.androidx.room.paging)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
