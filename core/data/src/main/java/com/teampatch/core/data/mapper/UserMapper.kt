672423017bdd6497d4373a8aeb45910ea05a085b
agvber <agvber12@gmail.com>
2025-02-25 01:07:01 +0900

    feat: UserOfflineRepositoryImpl 추가

--- core/data/src/main/java/com/teampatch/core/data/mapper/UserMapper.kt
+++ core/data/src/main/java/com/teampatch/core/data/mapper/UserMapper.kt
@@ -1,13 +1,17 @@
 package com.teampatch.core.data.mapper
 
+import com.harmony.core.database.model.UserEntity
 import com.teampatch.core.domain.model.Role
 import com.teampatch.core.domain.model.User
 import com.teampatch.core.network.model.user.ProfileResponse
 
+private const val MEMBER = "m"
+private const val VIP = "v"
+
 internal fun ProfileResponse.toDomain(): User {
     val role = when (user.permissionId) {
-        "v" -> Role.VIP
-        "m" -> Role.MEMBER
+        VIP -> Role.VIP
+        MEMBER -> Role.MEMBER
         else -> throw IllegalStateException()
     }
 
@@ -18,5 +22,34 @@ internal fun ProfileResponse.toDomain(): User {
         relation = "",
         profileImageUrl = null,
         role = role
+    )
+}
+
+internal fun UserEntity.toDomain(): User {
+    return User(
+        uid = uid.toString(),
+        groupId = groupId,
+        name = name,
+        relation = relation,
+        profileImageUrl = profileImageUri,
+        role = when (role) {
+            MEMBER -> Role.MEMBER
+            VIP -> Role.VIP
+            else -> throw IllegalArgumentException()
+        }
+    )
+}
+
+internal fun User.toEntity(): UserEntity {
+    return UserEntity(
+        uid = uid.toLong(),
+        groupId = groupId,
+        name = name,
+        relation = relation,
+        profileImageUri = profileImageUrl,
+        role = when (role) {
+            Role.VIP -> VIP
+            Role.MEMBER -> MEMBER
+        }
     )
 }
