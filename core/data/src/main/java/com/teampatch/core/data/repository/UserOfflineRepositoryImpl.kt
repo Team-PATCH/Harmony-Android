e9b6cd9152d8d2fd178578893e205656d699264c
agvber <agvber12@gmail.com>
2025-02-25 01:32:05 +0900

    fix: loggedInDebug build type에서 자동 로그인 되지 않는 문제 수정

--- core/data/src/main/java/com/teampatch/core/data/repository/UserOfflineRepositoryImpl.kt
+++ core/data/src/main/java/com/teampatch/core/data/repository/UserOfflineRepositoryImpl.kt
@@ -2,13 +2,16 @@ package com.teampatch.core.data.repository
 
 import com.harmony.core.database.dao.UserDao
 import com.harmony.core.database.model.UserEntity
+import com.teampatch.core.common.BuildConfig
 import com.teampatch.core.data.mapper.toDomain
+import com.teampatch.core.data.mapper.toEntity
 import com.teampatch.core.domain.model.User
 import com.teampatch.core.domain.repository.UserRepository
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.retryWhen
 import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
@@ -18,6 +21,14 @@ internal class UserOfflineRepositoryImpl @Inject constructor(
 
     override fun getUserInfo(): Flow<User> = userDao.getMyUserData()
         .map { it.toDomain() }
+        .retryWhen { cause, _ ->
+            if (IS_LOGGED_IN_DEBUG_MODE && cause is NullPointerException) {
+                val emptyUserEntity = User.createEmptyUser().copy(uid = "0").toEntity()
+                userDao.insertUsers(emptyUserEntity)
+                return@retryWhen true
+            }
+            false
+        }
 
     override suspend fun editProfile(
         name: String?,
@@ -34,5 +45,11 @@ internal class UserOfflineRepositoryImpl @Inject constructor(
         }
 
         userDao.updateUser(userEntity = userEntity)
+    }
+
+    companion object {
+        @Suppress("KotlinConstantConditions")
+        private const val IS_LOGGED_IN_DEBUG_MODE: Boolean =
+            BuildConfig.BUILD_TYPE == "loggedInDebug"
     }
 }
