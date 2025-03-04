ecbec50abadb5771e43f129e7525e6a6c43e293c
theBettor <be0128st@gmail.com>
2025-03-04 23:49:07 +0900

    fix: datePick말고 TimePick #105

--- feature/daily-edit/src/main/java/com/teampatch/feature/daily/edit/DailyEditScreen.kt
+++ feature/daily-edit/src/main/java/com/teampatch/feature/daily/edit/DailyEditScreen.kt
@@ -1,5 +1,6 @@
 package com.teampatch.feature.daily.edit
 
+import android.app.TimePickerDialog
 import android.widget.Toast
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
@@ -14,17 +15,14 @@ import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material3.DatePicker
-import androidx.compose.material3.DatePickerDialog
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.rememberDatePickerState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
@@ -50,12 +48,11 @@ import com.teampatch.core.designsystem.theme.WH
 import com.teampatch.core.designsystem.utils.noRippleClickable
 import com.teampatch.core.domain.fake.FakeDailyManage
 import com.teampatch.feature.daily.edit.R.string.btn_complete_daily
-import com.teampatch.feature.daily.edit.R.string.text_alarm_time
+import com.teampatch.feature.daily.edit.R.string.select_time
 import com.teampatch.feature.daily.edit.R.string.text_per_daily
 import com.teampatch.feature.daily.edit.R.string.title_daily
-import java.time.Instant
-import java.time.LocalDateTime
-import java.time.ZoneOffset
+import java.time.LocalTime
+import java.util.Calendar
 
 @Composable
 internal fun DailyEditRoute(
@@ -91,7 +88,31 @@ internal fun DailyEditRoute(
     }
 }
 
-@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun TimePickerDialog(
+    onDismissRequest: () -> Unit,
+    onConfirm: (hour: Int, minute: Int) -> Unit,
+) {
+    val context = LocalContext.current
+    val calendar = Calendar.getInstance()
+    val hour = calendar.get(Calendar.HOUR_OF_DAY)
+    val minute = calendar.get(Calendar.MINUTE)
+
+    val timePickerDialog = TimePickerDialog(
+        context,
+        { _, selectedHour, selectedMinute ->
+            onConfirm(selectedHour, selectedMinute)
+        },
+        hour,
+        minute,
+        true
+    )
+
+    LaunchedEffect(Unit) {
+        timePickerDialog.show()
+    }
+}
+
 @Composable
 internal fun DailyEditScreen(
     onDismissRequest: () -> Unit,
@@ -99,37 +120,8 @@ internal fun DailyEditScreen(
     uiState: DailyEditUiState,
 ) {
     var daily by rememberSaveable { mutableStateOf(uiState.dailyExpand.content) }
-    var dateTime: LocalDateTime? by rememberSaveable { mutableStateOf(null) }
-    var isDatePickerDialogShow by rememberSaveable { mutableStateOf(false) }
-
-    if (isDatePickerDialogShow) {
-        val datePickerState = rememberDatePickerState()
-        DatePickerDialog(
-            onDismissRequest = { isDatePickerDialogShow = false },
-            confirmButton = {
-                Text(
-                    text = stringResource(R.string.date_picker_ok),
-                    modifier = Modifier
-                        .padding(start = 16.dp, bottom = 12.dp, end = 12.dp)
-                        .noRippleClickable {
-                            val dateMillis =
-                                datePickerState.selectedDateMillis ?: return@noRippleClickable
-                            val instant = Instant.ofEpochMilli(dateMillis)
-                            dateTime = LocalDateTime.ofInstant(instant, ZoneOffset.UTC)
-                            isDatePickerDialogShow = false
-                        }
-                )
-            },
-            dismissButton = {
-                Text(
-                    text = stringResource(R.string.date_picker_cancel),
-                    modifier = Modifier.noRippleClickable { isDatePickerDialogShow = false }
-                )
-            }
-        ) {
-            DatePicker(state = datePickerState)
-        }
-    }
+    var time: LocalTime? by rememberSaveable { mutableStateOf(null) }
+    var isTimePickerDialogShow by remember { mutableStateOf(false) }
 
     Scaffold(
         topBar = {
@@ -178,11 +170,16 @@ internal fun DailyEditScreen(
             )
 
             Box(
-                modifier = Modifier.padding(start = 20.dp, end = 20.dp, top = 36.dp, bottom = 32.dp)
+                modifier = Modifier
+                    .padding(start = 20.dp, end = 20.dp, top = 36.dp, bottom = 32.dp)
             ) {
                 DefaultTextField(
                     value = daily,
-                    onValueChange = { if (it.length <= 200) daily = it },
+                    onValueChange = {
+                        if (it.length <= 200) {
+                            daily = it
+                        }
+                    },
                     singleLine = false,
                     keyboardOptions = KeyboardOptions(imeAction = ImeAction.None),
                     modifier = Modifier.height(IntrinsicSize.Max)
@@ -190,7 +187,7 @@ internal fun DailyEditScreen(
             }
 
             Text(
-                text = stringResource(text_alarm_time),
+                text = stringResource(select_time),
                 fontFamily = PretendardFontFamily,
                 color = BL,
                 fontSize = 18.sp,
@@ -206,19 +203,29 @@ internal fun DailyEditScreen(
                     .background(color = WH, shape = RoundedCornerShape(10.dp))
                     .border(width = 1.dp, color = G2, shape = RoundedCornerShape(10.dp))
                     .padding(horizontal = 20.dp)
-                    .noRippleClickable { isDatePickerDialogShow = true }
+                    .noRippleClickable { isTimePickerDialogShow = true }
             ) {
                 Image(
                     painter = painterResource(R.drawable.ic_date_memory_card),
-                    contentDescription = "date"
+                    contentDescription = "time"
                 )
                 Text(
-                    text = dateTime?.let { "${it.year}.${it.monthValue}.${it.dayOfMonth}" } ?: "",
+                    text = time?.let { "${it.hour}:${it.minute}" } ?: stringResource(select_time),
                     color = BL,
                     fontSize = 20.sp,
                     fontFamily = PretendardFontFamily,
                     fontWeight = FontWeight.Medium,
                     modifier = Modifier.padding(start = 20.dp)
+                )
+            }
+
+            if (isTimePickerDialogShow) {
+                TimePickerDialog(
+                    onDismissRequest = { isTimePickerDialogShow = false },
+                    onConfirm = { hour, minute ->
+                        time = LocalTime.of(hour, minute)
+                        isTimePickerDialogShow = false
+                    }
                 )
             }
         }
