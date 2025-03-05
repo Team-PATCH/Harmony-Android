624143ccd917cc6ab915b075f2edf960980e6809
theBettor <be0128st@gmail.com>
2025-03-05 16:10:03 +0900

    feat: dail-edit에 FilterChip 추가
    197~225번줄 코드를 추가하면 preview가 동작을 안합니다..!

--- feature/daily-edit/src/main/java/com/teampatch/feature/daily/edit/DailyEditViewModel.kt
+++ feature/daily-edit/src/main/java/com/teampatch/feature/daily/edit/DailyEditViewModel.kt
@@ -19,8 +19,7 @@ import kotlinx.coroutines.launch
 @HiltViewModel
 internal class DailyEditViewModel @Inject constructor(
     savedStateHandle: SavedStateHandle,
-    private val getDailyManageUsecase: GetDailyManageUseCase,
-    // usecase 만들어야될거같은데??
+    private val getDailyManageUseCase: GetDailyManageUseCase,
 ) : ViewModel() {
     private val dailyEditRoute = savedStateHandle.toRoute<DailyEditRoute>()
 
@@ -30,6 +29,9 @@ internal class DailyEditViewModel @Inject constructor(
     private val _sideEffect: Channel<DailyEditSideEffect> = Channel()
     val sideEffect: Flow<DailyEditSideEffect> = _sideEffect.receiveAsFlow()
 
+    var selectedDays = mutableStateOf(setOf<String>())
+        private set
+
     init {
         load()
     }
@@ -38,18 +40,27 @@ internal class DailyEditViewModel @Inject constructor(
         try {
             if (dailyEditRoute.dailyId.isEmpty()) return@launch
 
-            val expand = getDailyManageUsecase(dailyEditRoute.dailyId)
+            val expand = getDailyManageUseCase(dailyEditRoute.dailyId)
             dailyEditUiState.value = DailyEditUiState(
                 dailyExpand = expand,
                 isLoading = false
             )
+            selectedDays.value = setOf(expand.dateTime.dayOfWeek.name)
         } catch (e: Exception) {
             _sideEffect.send(DailyEditSideEffect.LoadError(e))
             e.printStackTrace()
         }
     }
 
+    fun toggleDaySelection(day: String) {
+        selectedDays.value = if (selectedDays.value.contains(day)) {
+            selectedDays.value - day
+        } else {
+            selectedDays.value + day
+        }
+    }
+
     fun saveDaily(daily: String) = viewModelScope.launch {
-        // usecase 만들어ㅑㅇ될거같은데???
+        // UseCase를 호출하여 선택한 요일과 함께 저장
     }
 }
