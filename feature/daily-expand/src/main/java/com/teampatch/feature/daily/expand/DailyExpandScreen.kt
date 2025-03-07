9c0d810c2bca75a904d0e23662d5dfe3e5923c1e
theBettor <be0128st@gmail.com>
2025-03-07 21:23:13 +0900

    feat: 일과 수정 및 삭제할 수 있는 DropdownMenu 추가 #103

--- feature/daily-expand/src/main/java/com/teampatch/feature/daily/expand/DailyExpandScreen.kt
+++ feature/daily-expand/src/main/java/com/teampatch/feature/daily/expand/DailyExpandScreen.kt
@@ -2,7 +2,9 @@ package com.teampatch.feature.daily.expand
 
 import android.widget.Toast
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
@@ -11,19 +13,27 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.widthIn
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.DpOffset
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -38,8 +48,8 @@ import com.teampatch.core.designsystem.theme.G3
 import com.teampatch.core.designsystem.theme.G4
 import com.teampatch.core.designsystem.theme.HarmonyTheme
 import com.teampatch.core.designsystem.theme.PretendardFontFamily
-import com.teampatch.core.designsystem.utils.noRippleClickable
 import com.teampatch.core.domain.fake.FakeDailyManage
+import com.teampatch.core.domain.model.DailyManage
 import com.teampatch.feature.daily.expand.model.DailyExpandSideEffect
 import com.teampatch.feature.daily.expand.model.DailyExpandUiState
 import kotlinx.coroutines.flow.flowOf
@@ -47,6 +57,8 @@ import kotlinx.coroutines.flow.flowOf
 @Composable
 internal fun DailyExpandRoute(
     onBackRequest: () -> Unit,
+    onEditClick: (DailyManage) -> Unit,
+    onDeleteClick: (DailyManage) -> Unit,
     viewModel: DailyExpandViewModel = hiltViewModel(),
 ) {
     val context = LocalContext.current
@@ -55,6 +67,8 @@ internal fun DailyExpandRoute(
     if (!uiState.isLoading) {
         DailyExpandScreen(
             onBackRequest = onBackRequest,
+            onEditClick = onEditClick,
+            onDeleteClick = onDeleteClick,
             uiState = uiState
         )
     }
@@ -72,6 +86,8 @@ internal fun DailyExpandRoute(
 @Composable
 internal fun DailyExpandScreen(
     onBackRequest: () -> Unit,
+    onEditClick: (DailyManage) -> Unit,
+    onDeleteClick: (DailyManage) -> Unit,
     uiState: DailyExpandUiState,
 ) {
     val daily = uiState.dailyManage.collectAsLazyPagingItems()
@@ -92,46 +108,110 @@ internal fun DailyExpandScreen(
                 .padding(top = 16.dp)
         ) {
             items(daily.itemCount) { index ->
-                Column(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(start = 20.dp, end = 20.dp, bottom = 12.dp)
-                        .background(G1, RoundedCornerShape(10.dp))
-                        .noRippleClickable {
-                            val id = daily.getOrNull(index)?.id ?: return@noRippleClickable
-                        }
-                ) {
-                    Text(
-                        text = "#${daily.getOrNull(index)?.number}",
-                        fontFamily = PretendardFontFamily,
-                        fontWeight = FontWeight.SemiBold,
-                        fontSize = 18.sp,
-                        color = G4,
-                        modifier = Modifier
-                            .padding(top = 20.dp, start = 28.dp, bottom = 4.dp)
-                    )
-                    Row(
-                        horizontalArrangement = Arrangement.SpaceBetween,
-                        verticalAlignment = Alignment.CenterVertically,
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .padding(start = 24.dp, end = 24.dp, bottom = 16.dp)
-                    ) {
+                val item = daily.getOrNull(index) ?: return@items
+                DailyItem(
+                    dailyItem = daily.getOrNull(index),
+                    onEditClick = { daily.getOrNull(index)?.let { onEditClick(it) } },
+                    onDeleteClick = { daily.getOrNull(index)?.let { onDeleteClick(it) } }
+                )
+            }
+        }
+    }
+}
+
+@Composable
+fun DailyItem(
+    dailyItem: DailyManage?,
+    onEditClick: () -> Unit,
+    onDeleteClick: () -> Unit,
+) {
+    var expanded by remember { mutableStateOf(false) }
+    if (dailyItem == null) return
+
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 20.dp, vertical = 12.dp)
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(G1, RoundedCornerShape(10.dp))
+                .clickable { expanded = !expanded } // 클릭 시 expanded 상태 변경
+                .padding(12.dp)
+        ) {
+            Row(
+                horizontalArrangement = Arrangement.SpaceBetween,
+                verticalAlignment = Alignment.CenterVertically,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(start = 24.dp, end = 24.dp, bottom = 16.dp)
+            ) {
+                Text(
+                    text = "#${dailyItem.number}",
+                    fontFamily = PretendardFontFamily,
+                    fontWeight = FontWeight.SemiBold,
+                    fontSize = 18.sp,
+                    color = G4
+                )
+                Icon(
+                    painter = painterResource(id = ic_daily_edit),
+                    contentDescription = "daily_edit",
+                    tint = G3,
+                    modifier = Modifier.clickable { expanded = !expanded } // 클릭하면 메뉴 활성화
+                )
+            }
+
+            Row(
+                horizontalArrangement = Arrangement.SpaceBetween,
+                verticalAlignment = Alignment.CenterVertically,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(start = 24.dp, end = 24.dp, bottom = 16.dp)
+            ) {
+                Text(
+                    text = dailyItem.title,
+                    fontFamily = PretendardFontFamily,
+                    fontWeight = FontWeight.SemiBold,
+                    fontSize = 20.sp,
+                    color = BL,
+                    modifier = Modifier.widthIn(max = 240.dp)
+                )
+            }
+            // DropdownMenu 추가
+            DropdownMenu(
+                expanded = expanded,
+                onDismissRequest = { expanded = false },
+                offset = DpOffset(250.dp, -80.dp), // 오른쪽으로 이동
+                modifier = Modifier.background(Color.White)
+
+            ) {
+                DropdownMenuItem(
+                    text = {
                         Text(
-                            text = daily.getOrNull(index)?.title ?: "",
-                            fontFamily = PretendardFontFamily,
-                            fontWeight = FontWeight.SemiBold,
-                            fontSize = 20.sp,
-                            color = BL,
-                            modifier = Modifier.widthIn(max = 240.dp)
+                            "일과 수정",
+                            modifier = Modifier.fillMaxWidth(),
+                            textAlign = TextAlign.Center
                         )
-                        Icon(
-                            painter = painterResource(ic_daily_edit),
-                            contentDescription = "daily_edit",
-                            tint = G3
-                        )
+                    },
+                    onClick = {
+                        expanded = false
+                        onEditClick()
                     }
-                }
+                )
+                DropdownMenuItem(
+                    text = {
+                        Text(
+                            "일과 수정",
+                            modifier = Modifier.fillMaxWidth(),
+                            textAlign = TextAlign.Center
+                        )
+                    },
+                    onClick = {
+                        expanded = false
+                        onDeleteClick()
+                    }
+                )
             }
         }
     }
@@ -143,6 +223,8 @@ private fun DailyExpandScreenPreview() {
     HarmonyTheme {
         DailyExpandScreen(
             onBackRequest = {},
+            onEditClick = {},
+            onDeleteClick = {},
             uiState = DailyExpandUiState(dailyManage = flowOf(PagingData.from(listOf(FakeDailyManage().get()))))
         )
     }
