54becc1b64a6891ea2c5b3341e2bae345a33acb4
theBettor <be0128st@gmail.com>
2025-03-28 01:55:11 +0900

    fix: 안쓰는 drawble 삭제, 화면연결 등 남은숙제 TODO로 기록

--- feature/onboarding-enter/src/main/java/com/teampatch/feature/onboarding/enter/OnboardingEnterSpaceScreen.kt
+++ feature/onboarding-enter/src/main/java/com/teampatch/feature/onboarding/enter/OnboardingEnterSpaceScreen.kt
@@ -3,10 +3,8 @@ package com.teampatch.feature.onboarding.enter
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
@@ -19,31 +17,24 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringArrayResource
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.buildAnnotatedString
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.teampatch.core.designsystem.R.drawable.ic_my_appbar
 import com.teampatch.core.designsystem.component.DefaultButton
 import com.teampatch.core.designsystem.component.OnBoardingLayout
-import com.teampatch.core.designsystem.component.SpeechBubble
 import com.teampatch.core.designsystem.theme.BL
 import com.teampatch.core.designsystem.theme.HarmonyTheme
 import com.teampatch.core.designsystem.theme.MainGreen
-import com.teampatch.core.designsystem.theme.PretendardFontFamily
 import com.teampatch.feature.onboarding.enter.R.array.title_onboarding_enter_space
 import com.teampatch.feature.onboarding.enter.R.drawable.img_onboarding_enter_space
-import com.teampatch.feature.onboarding.enter.R.string.text_onboarding_enter_speech_bubble
 
 @Composable
 fun OnboardingEnterSpaceScreen(
     onBackRequest: () -> Unit,
-    onHomeRouteRequest: () -> Unit,
+    onHomeRouteRequest: () -> Unit, /* TODO */
 ) {
     OnBoardingLayout(
         title = buildAnnotatedString {
