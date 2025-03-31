6814d241451ba2e49f19d830b4b397dfbca255eb
theBettor <be0128st@gmail.com>
2025-03-31 21:42:24 +0900

    fix: lint

--- feature/onboarding-make/src/main/java/com/teampatch/feature/onboarding/make/OnboardingMakeViewModel.kt
+++ feature/onboarding-make/src/main/java/com/teampatch/feature/onboarding/make/OnboardingMakeViewModel.kt
@@ -6,10 +6,10 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
-import javax.inject.Inject
 
 @HiltViewModel
 class OnboardingMakeViewModel @Inject constructor() : ViewModel() {
