73795fdcc6e4d96a57b6b82f7c945de65e712483
agvber <agvber12@gmail.com>
2025-04-30 08:57:35 +0900

    feat: db에 저장될 datetime 형식 변수 선언

---
+++ core/database/src/main/java/com/harmony/core/database/Utils.kt
@@ -0,0 +1,6 @@
+package com.harmony.core.database
+
+import java.time.format.DateTimeFormatter
+
+val LOCAL_DB_DATE_TIME_FORMATTER: DateTimeFormatter =
+    DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")
