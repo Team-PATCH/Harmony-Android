6864c682a941d1acbd8038ff223c7b99f5012d42
agvber <agvber12@gmail.com>
2025-04-30 18:48:18 +0900

    feat: TodoEntity dateTime 포멧 변경
    epoch 시간에서 "yyyy-MM-dd HH:mm:ss" 해당 문자열 패턴으로 변경

--- core/data/src/main/java/com/teampatch/core/data/mapper/TodoMapper.kt
+++ core/data/src/main/java/com/teampatch/core/data/mapper/TodoMapper.kt
@@ -1,14 +1,13 @@
 package com.teampatch.core.data.mapper
 
+import com.harmony.core.database.LOCAL_DB_DATE_TIME_FORMATTER
 import com.harmony.core.database.model.TodoEntity
 import com.teampatch.core.domain.model.Todo
-import java.time.Instant
 import java.time.LocalDateTime
-import java.time.ZoneId
 
 fun TodoEntity.toDomain() = Todo(
     id = id.toString(),
-    dateTime = LocalDateTime.ofInstant(Instant.ofEpochMilli(epochMilli), ZoneId.systemDefault()),
+    dateTime = LocalDateTime.parse(createdAt, LOCAL_DB_DATE_TIME_FORMATTER),
     title = title,
     isFinished = isFinished
 )
