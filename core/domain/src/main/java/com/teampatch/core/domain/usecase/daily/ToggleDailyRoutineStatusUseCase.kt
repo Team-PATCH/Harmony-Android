12fc7bf058ccfc5247695648bd965fc54b8cf15d
agvber <agvber12@gmail.com>
2025-04-30 18:57:28 +0900

    feat: 할일 완료 상태를 변경하는 기능 추가하였습니다.

--- core/domain/src/main/java/com/teampatch/core/domain/usecase/daily/ToggleDailyRoutineStatusUseCase.kt
+++ core/domain/src/main/java/com/teampatch/core/domain/usecase/daily/ToggleDailyRoutineStatusUseCase.kt
@@ -1,12 +1,16 @@
 package com.teampatch.core.domain.usecase.daily
 
+import com.teampatch.core.domain.repository.TodoRepository
 import javax.inject.Inject
 
-class ToggleDailyRoutineStatusUseCase @Inject constructor() {
+class ToggleDailyRoutineStatusUseCase @Inject constructor(
+    private val todoRepository: TodoRepository,
+) {
 
     suspend operator fun invoke(
         id: String,
         isFinished: Boolean,
     ) {
+        todoRepository.toggleTodoStatus(id, isFinished)
     }
 }
