b5be3c3780eb9c634a71762cea3c110feb4304d0
agvber <agvber12@gmail.com>
2025-05-14 14:09:48 +0900

    feat: 추억저장소 화면이 db의 데이터를 가져올 수 있도록 로직을 변경합니다

--- feature/memorystorage/src/main/java/com/teampatch/feature/memorystorage/MemoryStorageUiState.kt
+++ feature/memorystorage/src/main/java/com/teampatch/feature/memorystorage/MemoryStorageUiState.kt
@@ -1,11 +1,9 @@
 package com.teampatch.feature.memorystorage
 
-import com.teampatch.core.domain.model.MemoryCard
-
 sealed interface MemoryStorageUiState {
-    object Loading : MemoryStorageUiState
+    data object Loading : MemoryStorageUiState
 
-    data class Success(val memories: Map<String, MemoryCard>) : MemoryStorageUiState
+    data object Success : MemoryStorageUiState
 
     data class Error(val message: String) : MemoryStorageUiState
 }
--- feature/memorystorage/src/main/java/com/teampatch/feature/memorystorage/MemoryStorageViewModel.kt
+++ feature/memorystorage/src/main/java/com/teampatch/feature/memorystorage/MemoryStorageViewModel.kt
@@ -1,45 +1,27 @@
 package com.teampatch.feature.memorystorage
 
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
-import com.teampatch.core.domain.usecase.memory.GetLatestMemoryCardUseCase
-import com.teampatch.core.domain.usecase.memory.GetMemoryCardUseCase
+import androidx.paging.PagingData
+import com.teampatch.core.common.flowErrorCatch
+import com.teampatch.core.domain.model.MemoryCard
+import com.teampatch.core.domain.usecase.memory.GetMemoryCardsUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
-import javax.inject.Inject
-import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
-import kotlinx.coroutines.flow.receiveAsFlow
-import kotlinx.coroutines.launch
+import javax.inject.Inject
 
 @HiltViewModel
 internal class MemoryStorageViewModel @Inject constructor(
-    private val getLatestMemoryCardUseCase: GetLatestMemoryCardUseCase,
-    private val getMemoryCardUseCase: GetMemoryCardUseCase,
+    private val getMemoryCardsUseCase: GetMemoryCardsUseCase,
 ) : ViewModel() {
 
-    private val _event: Channel<MemoryStorageEvent> = Channel()
-    val event: Flow<MemoryStorageEvent> = _event.receiveAsFlow()
-
-    private val _memoryStorageUiState = MutableStateFlow<MemoryStorageUiState>(MemoryStorageUiState.Loading)
+    private val _memoryStorageUiState =
+        MutableStateFlow<MemoryStorageUiState>(MemoryStorageUiState.Loading)
     val memoryStorageUiState = _memoryStorageUiState.asStateFlow()
 
-    init {
-        loadData()
-    }
-
-    private fun loadData() = viewModelScope.launch {
-        _memoryStorageUiState.value = MemoryStorageUiState.Loading
-
-        try {
-            val memoryCard = getMemoryCardUseCase("someId") // 예제 코드
-            _memoryStorageUiState.value = MemoryStorageUiState.Success(
-                memories = mapOf(memoryCard.id to memoryCard)
-            )
-        } catch (e: Exception) {
-            _memoryStorageUiState.value = MemoryStorageUiState.Error("데이터 로딩 실패")
-            _event.send(MemoryStorageEvent.LoadError)
-        }
-    }
+    val memoryCards: Flow<PagingData<MemoryCard>> = flowErrorCatch(
+        block = { getMemoryCardsUseCase() },
+        action = { it.printStackTrace() }
+    )
 }
