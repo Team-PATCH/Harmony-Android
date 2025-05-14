57c16db91c9339fc8b0c0ef12e46baffc9c4b6c5
agvber <agvber12@gmail.com>
2025-05-14 11:46:11 +0900

    refactor: MemoryCard domain model imageUrl type nullable 형태로 변경
    Changed the type of `imageUrl` in the `MemoryCard` data class from `String` to `String?` to allow for nullable image URLs.

--- core/domain/src/main/java/com/teampatch/core/domain/model/MemoryCard.kt
+++ core/domain/src/main/java/com/teampatch/core/domain/model/MemoryCard.kt
@@ -7,6 +7,6 @@ data class MemoryCard(
     val writerTitle: String,
     val writerName: String,
     val text: String,
-    val imageUrl: String,
+    val imageUrl: String?,
     val dateTime: LocalDateTime,
 )
