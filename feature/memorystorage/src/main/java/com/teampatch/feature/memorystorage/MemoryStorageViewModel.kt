10dd1069e833b0afe4abf47a5dfee9f6e1153f9e
theBettor <be0128st@gmail.com>
2025-06-10 00:30:20 +0900

    fix: 안쓰는 필드 삭제 #144

--- feature/memorystorage/src/main/java/com/teampatch/feature/memorystorage/MemoryStorageViewModel.kt
+++ feature/memorystorage/src/main/java/com/teampatch/feature/memorystorage/MemoryStorageViewModel.kt
@@ -30,9 +30,6 @@ internal class MemoryStorageViewModel @Inject constructor(
     private val _searchQuery = MutableStateFlow("")
     val searchQuery: StateFlow<String> = _searchQuery.asStateFlow()
 
-    private val _sortOption = MutableStateFlow("오래된순")
-    val sortOption: StateFlow<String> = _sortOption.asStateFlow()
-
     val memoryCards: Flow<PagingData<MemoryCard>> =
         _searchQuery
             .debounce(300)
